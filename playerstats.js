b4afcad2aeb21057cf4fffd24670671fec8ad653
aleksfasting <aleks.fasting@gmail.com>
2022-09-28 01:51:25 +0200

    completed table for teamstats

--- playerstats.js
+++ playerstats.js
@@ -1,6 +1,6 @@
 
 function showPlayerEl(doc) { // Funksjon for å vise spillerne i tabell
-    let rounds = doc.data().goalsInRound.length // antall runder kamper spilt
+    
     let tbodyEl = document.querySelector('tbody')
     let trEl = document.createElement('tr')
 
@@ -8,29 +8,39 @@ function showPlayerEl(doc) { // Funksjon for å vise spillerne i tabell
     tdEl.innerHTML = doc.data().name
     trEl.appendChild(tdEl)
 
-    tdEl = document.createElement('td') // for poeng i siste kampen
-    tdEl.innerHTML = calcPointsRound(rounds-1,doc.data().goalsInRound,doc.data().assistsInRound,doc.data().MOTM,doc.data().CSInRound)
-    trEl.appendChild(tdEl)
+    if (doc.data().goalsInRound != undefined) {
+        let rounds = doc.data().goalsInRound.length
 
-    tdEl = document.createElement('td') // for poeng totalt
-    tdEl.innerHTML = calcPoints(rounds, doc.data().goalsInRound, doc.data().assistsInRound, doc.data().MOTM, doc.data().CSInRound)
-    trEl.appendChild(tdEl)
+        tdEl = document.createElement('td') // for poeng i siste kampen
+        tdEl.innerHTML = calcPointsRound(rounds-1,doc.data().goalsInRound,doc.data().assistsInRound,doc.data().MOTM,doc.data().CSInRound)
+        trEl.appendChild(tdEl)
 
-    tdEl = document.createElement('td') // for antall mål
-    tdEl.innerHTML = countFunc(doc.data().goalsInRound)
-    trEl.appendChild(tdEl)
+        tdEl = document.createElement('td') // for poeng totalt
+        tdEl.innerHTML = calcPoints(rounds, doc.data().goalsInRound, doc.data().assistsInRound, doc.data().MOTM, doc.data().CSInRound)
+        trEl.appendChild(tdEl)
 
-    tdEl = document.createElement('td') // antall assists
-    tdEl.innerHTML = countFunc(doc.data().assistsInRound)
-    trEl.appendChild(tdEl)
+        tdEl = document.createElement('td') // for antall mål
+        tdEl.innerHTML = countFunc(doc.data().goalsInRound)
+        trEl.appendChild(tdEl)
 
-    tdEl = document.createElement('td') // antall clean sheets
-    tdEl.innerHTML = countFunc(doc.data().CSInRound)
-    trEl.appendChild(tdEl)
+        tdEl = document.createElement('td') // antall assists
+        tdEl.innerHTML = countFunc(doc.data().assistsInRound)
+        trEl.appendChild(tdEl)
 
-    tdEl = document.createElement('td') // antall Man of the Match
-    tdEl.innerHTML = countFunc(doc.data().MOTM)
-    trEl.appendChild(tdEl)
+        tdEl = document.createElement('td') // antall clean sheets
+        tdEl.innerHTML = countFunc(doc.data().CSInRound)
+        trEl.appendChild(tdEl)
+
+        tdEl = document.createElement('td') // antall Man of the Match
+        tdEl.innerHTML = countFunc(doc.data().MOTM)
+        trEl.appendChild(tdEl)
+    } else {
+        for (i=0;i<6;i++) {
+            tdEl = document.createElement('td')
+            tdEl.innerHTML = 0
+            trEl.appendChild(tdEl)
+        }
+    }
 
     tdEl = document.createElement('td') // posisjonen til spilleren
     tdEl.innerHTML = posFunc(doc.data().keeper)
--- teamstats.js
+++ teamstats.js
@@ -11,6 +11,32 @@ function showTeamEl(doc) {
     tdEl.innerHTML = doc.data().usrnm
     trEl.appendChild(tdEl)
 
+
+    db.collection('players').get().then((snapshot) => {
+        let documents1 = snapshot.docs;
+        gwtot = 0
+        tot = 0
+        for (k=0;k<documents1.length;k++) {
+            if (documents1[k].data().goalsInRound == undefined) {
+                rounds = 0
+            } else {
+                rounds = documents1[k].data().goalsInRound.length
+            }
+            doc1 = documents1[k].data()
+            if (doc.data().players.includes(doc1.name)) {
+                gwtot += calcPointsRound(rounds - 1, doc1.goalsInRound, doc1.assistsInRound, doc1.MOTM, doc1.CSInRound)
+                tot += calcPoints(rounds, doc1.goalsInRound, doc1.assistsInRound, doc1.MOTM, doc1.CSInRound)
+            }
+        }
+        tdEl = document.createElement('td')
+        tdEl.innerHTML = gwtot
+        trEl.appendChild(tdEl)
+
+        tdEl = document.createElement('td')
+        tdEl.innerHTML = tot
+        trEl.appendChild(tdEl)
+    })
+
     tbodyEl.appendChild(trEl)
 }
 
